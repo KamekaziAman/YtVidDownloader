757578b89a7490be72a970d6539aecbe5b5d272c
Aman <amanrai02122004@gmail.com>
2025-10-28 17:25:39 +0530

    site3

--- YtVidDownloader/YtVidDownloader/settings.py
+++ YtVidDownloader/YtVidDownloader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-pt!)*p+u!t29v+g^7n@0suj+34df((&7ela09-d4^$=ho)ys%1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
